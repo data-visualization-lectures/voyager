a4f02d9f6fe22c0b91fd4b58a386ddbeb221549d
Yuichi Yazaki <visualizingjp@gmail.com>
2025-12-20 08:23:06 +0900

    不要な画像（ロゴ）を削除し、「プロジェクト・ファイルの読込」「プロジェクト・ファイルの保存」ボタンを配置
    ボタンはまだ機能しない

--- src/components/header/controls.tsx
+++ src/components/header/controls.tsx
@@ -2,14 +2,14 @@ import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
 import {BookmarkPane} from '../bookmark';
 import * as styles from './controls.scss';
-import { UndoRedo } from './undo-redo';
+import {UndoRedo} from './undo-redo';
 
 export class ControlsBase extends React.PureComponent<{}, {}> {
   public render() {
     return (
       <div styleName='controls'>
-        <BookmarkPane/>
-        <UndoRedo/>
+        <BookmarkPane />
+        <UndoRedo />
       </div>
     );
   }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as CSSModules from 'react-css-modules';
 import {connect} from 'react-redux';
 import {InlineData} from 'vega-lite/build/src/data';
-import * as idlLogo from '../../../images/idl-h56.png';
+
 import * as logo from '../../../images/logo.png';
 import {State} from '../../models/index';
 import {selectData} from '../../selectors/dataset';
@@ -19,18 +19,21 @@ export class HeaderBase extends React.PureComponent<HeaderProps, {}> {
 
     return (
       <div styleName='header'>
-        <img styleName='voyager-logo' src={logo}/>
-        {data && <Controls/>}
-        <a styleName='idl-logo' onClick={this.openLink}>
-          <img src={idlLogo}/>
-        </a>
+        <img styleName='voyager-logo' src={logo} />
+        {data && <Controls />}
+        <div styleName='right-controls'>
+          <button className="command">
+            <i className="fa fa-folder-open" /> プロジェクト・ファイルの読込
+          </button>
+          <button className="command">
+            <i className="fa fa-floppy-o" /> プロジェクト・ファイルの保存
+          </button>
+        </div>
       </div>
     );
   }
 
-  private openLink() {
-    window.open('https://idl.cs.washington.edu/');
-  }
+
 }
 
 export const Header = connect(
